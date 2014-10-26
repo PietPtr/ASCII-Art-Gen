4d090655f3e69ae5f89735ff9b83a96bb6a283ff
Pietdagamer <Pietdagamer@users.noreply.github.com>
2014-10-26 21:22:29 +0100

    Fixed very important commenting issue

--- Program.cs
+++ Program.cs
@@ -1,3 +1,81 @@
+/*
+ *  ################################################################################################
+    ################################################################################################
+    ###############################################################%`cc`p%##########################
+    ############################################################*cgGcccccGLfY#######################
+    ###########################################################`gcccccccccccGf%#####################
+    #########################################################*fUccccccccccccccf`####################
+    ########################################################XgcccccccccccccccccfU###################
+    #######################################################Xgcccccccccccccccccccf`##################
+    ######################################################*gcccccccccccccccccccccfX#################
+    #####################################################WgcccccccccccccccccccccccG#################
+    #####################################################cUcccccccccccccccccccccccf0################
+    ###################################################*Ufccccccccccccccccccccccccc`################
+    ##################################################*gcccccccccccccccccccccccccccf################
+    ##################################################ccccffcccccccccccccccccccccccf################
+    ##################################################fSSS8fcccccccccGcccccccccccccf################
+    #################################################cGcccccccccccccG8cccccccccccccf################
+    ################################################0fcccccccccccccccfLcccccccccccc`################
+    ################################################cgf__fLfULcUScccccG_ccccccccccg*################
+    ################################################cW#`=#WYf_cSUccccccfScccccccccG#################
+    ################################################=########Lcf=`UgGcccLfcccccccGY#################
+    ################################################p#######XfU;###fHUgccScccccccf##################
+    ################################################*Y#####WfcU=###WW#*GfSccccccfX##################
+    #################################################f;###*gUccU########c_ccccccf###################
+    ################################################WgGfUgOUcccg*#######0gcccccg*###################
+    ################################################0fcUSLccccccfW######fcccccU`####################
+    ################################################0SSSUcccccccUg%###*GUcccccgW####################
+    #####*%########################################%ffccccccccccGLGfUfgccccccf;#####################
+    ####*ggX######################################`fcccccccccccccfSUcccccccccf######################
+    ####fcc`#####################################`fcccccccccccccccULSgccccccf;######################
+    ###Xfccf####################################;Gccfcccccccccccccccccccccccg#######################
+    ###fcccg###################################Wgccc_ccccccccccccccccccccccc`#######################
+    ##Wgcccf=#############;X###################`UccSUccGcccccccccccccccccccg*#######################
+    ##=UccccGS*###########cg%##################gccUSccfLcccfcccccccccccccccf########################
+    ##UccccccUp###########Ucg*################XGccLfccScccc_cccccccccccccccc########################
+    ##gcccccccf###########fccG################pUccSccUScccffcccfHBUcccccccf;########################
+    #Wgcccccccg;0#########fccGY###############`cccSccgfcccScccc?==bcccccccg*########################
+    #0fccccccccGg########WgcccG###############;UcULccSccccScccg?==OcccccccL#########################
+    #%Gcccccccccf0#######0fcccg*##############*gccSccScccfLcccScUgfcccccccG#########################
+    #0fcccccccccU=`0#####pUcccUp###############cfcSccScccfGcccScccccccccccG#########################
+    #Wgccccccccccgff#####fcccccc################pfgfcScccfGcccScccccccccccLcW#######################
+    ##gccccccccccccG%###WgcccccG###################cgOfccGgcccSccccccccccfpWc#######################
+    ##UcccccccccccccfW##`Ucccccf###################0`S_Gf__ccgfccccccccccg*WYX######################
+    ##=UccccccccccccUgGWgccccccf##################X;WLcfgUfSSLccccccccccS`WWW`######################
+    ##WgccccccccccccccUbUccccccf################Wp;W0Sgcccccccccccccccggf*WWWpW#####################
+    ###UcccccccccccccccScccccccf##############Wpp*WW;;=fLfUcccccccUfLf=`YWWWW*`#####################
+    ###0fccccccccccccccgcccccccc#############;`*WWWW`YYYYpSGfffffGU=YY;cWWWWWWX=####################
+    ####UUccccccccccccccccccccfY###########0f0WWWWWW`YYYYYfYYYYYYYYYYYf*WWWWWWW%;###################
+    ####Wgccccccccccccccccccccg###########pc;WWWWWWWcUYYYYfYYYYYYYYYYU0WWWWWWWWWY%##################
+    #####%fccccccccccccccccccU`#########*c0`WWWWWWWW``G;Y;cYYYYYYYY=gYWWWWWWWWWWW=0#################
+    ######=Gcccccccccccccccccg*########YpW*`WWWWWWWW`YYUGS`YYYY;cffL;WWWWWWWWWWWWW`W################
+    #######pGccccccccccccccccG########p%WW;0WWWWWWWWp;YYY;`UffUc;YcYWWWWWWWWWWWWWWWc################
+    ########=fcccccccccccccccLcW####Wc0WWW`WWWWWWWWWY=YYYYYYYYYYYU%WWWWWWWWWWWWWWWW0p###############
+    #########YgccccccccccccccS*=###*c*WWWW`WWWWWWWWW0cYYYYYYY=Y;f0WWWWWWWWWWWWWWW=`pUX##############
+    ##########*fUcccccccccccL%Wp##*`WWWWW%YWWWWWWWWWWUYYYYYYGgL_*WWWWWWWWWWWWWWWcXWWW`W#############
+    ###########WcGccccccccfG0WWp#0pWWWWWW`*WWWWWWWWWWUYYYYYYfGcUg;WWWWWWWWWWWWW`*WWWW*`#############
+    ############WGgccccUgGYWWWWY;pWWWWWWW`WWWWWWWWWWWcYYYYYf;fcccGU*WWWWWWWWWWYYWWWWWWXY############
+    ############;*XbgfUp*WWWWWW*GWWWWWWWW`WWWWWWWWWWW=;YY`c*W;GcccUf*WWWWWWWWW`WWWWWWWWp*###########
+    ############pW*0*WWWWWWWWWWW`WWWWWWW*pWWWWWWWWWWW*c;f%WWWWfccccUf*WWWWWWW;XWWWWWWWW*`###########
+    ############=*WWWWWWWWWWWWWW`WWWWWWWY%WWWWWWWWWWWWgpWWWWWW=fccccUUWWWWWWW`WWWWWWWWWW%%##########
+    ############*;WWWWWWWWWWWWWW;*WWWWWWp*WWWWWWWWWWYc0WWWWWWW*gcccccfYWWWWW*pWWWWWWWWWWW`##########
+    #############`WWWWWWWWWWWWWWWWWWWWWW`WWWWWWWWW0cYWWWWWWWWWWGccccccGWWWWW;0WWWWWWWWWWW*=#########
+    #############=*WWWWWWWWWWWWWWWWWWWWW`WWWWWWW*`=WWWWWWWWWWWWGccccccfXWWWW`WWWWWWWWWWWWWp*########
+    #############*=WWWWWWWWWWWWWWWWWWWWW`WWWWW*p`*WWWWWWWWWWWWWGccccccccWWWW`WWWWWWWWWWWWW*`########
+    ##############`WWWWWWWWWWWWWWWWWWWWW`WWWW;`0WWWWWWWWWWWWWWWGcccccccfWWWW`WWWWWWWWWWWWWW=*#######
+    ##############pWWWWWWWWWWWWWWWWWWWW*pWWW0=WWWWWWWWWWWWWWWWWfcccccccL*WW*pWWWWWWWWWWWWWW*p#######
+    ##############;*WWWWWWWWWWWWWWWWWWW0;WWWp*WWWWWWWWWWWWWWWWWccccccccf;WW%YWWWWWWWWWWWWWWWpW######
+    ##############*;WWWWWWWWWWWWWWWWWWW%%WWW`WWX*WWWWWWWWWWWWWW=fccccccc`WW;0WWWWWWWWWWWWWWW*=######
+    ###############`WWWWWWWWWWWWWWWWWWW;0WWW`W`Yc*WWWWWWWWWWWWW0_cccccccUWW=*WWWWWWWWWWWWWWWW`######
+    ###############pWWWWWWWWWWWWWWWWWWWp*WWW`W`W0;WWWWWWWWWWW0fgGcccccccf*Wp*WWWWWWWWWWWWWWWW;*#####
+    ###############Y0WWWWWWWWWWWWWWWWWW`WWWW`W`0`*WWWWWWWWWW*gccccccccccS``fWWWWWWWWWWWWWWWWW*;#####
+    ################pWWWWWWWWWWWWWWWWWW`WWWW`W*;0WWWWWWWWpffgfccccccccccG;W%cYWWWWWWWWWWWWWWWWp#####
+    ################`WWWWWWWWWWWWWWWWWW`WWWW`WWWWWWWW*;Y%gccccccccccccccccWWWY`p*WWWWWWWWWWWWW`#####
+    ################*;WWWWWWWWWWWWWWWWY%WWWW`WWWW*X0WUfGfUccccccccccccccc`WWWWW*=``XWWWWWWWWWW;*####
+    #################`WWWWWWWWWWWWWWWWcWWWW*gc`p;LfggfccccccccccccccccccUpWWWWWWWWW*WWWWWWWWWW0Y####
+*/
+
+
 using System;
 using System.IO;
 using System.Collections.Generic;
@@ -18,7 +96,7 @@ namespace ASCII_art
 
         static Random rnd = new Random();
 
-        static int iconWidth = 12;
+        static int iconWidth = 13;
         static int iconHeight = 13;
 
         static int randint(int low, int high)
@@ -143,6 +221,9 @@ namespace ASCII_art
 
             iconList.Sort();
 
+            Console.WriteLine("Recommend resolutions: ");
+            Console.WriteLine(" - For Reddit: max 105x141");
+            Console.WriteLine(" - For Others: max 1920x1080 for drawing, text will work for 4K");
             Console.Write("Filename of the source picture: ");
             string filename = Console.ReadLine();
 
